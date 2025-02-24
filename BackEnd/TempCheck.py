73f7dce211d98403db16383a97f74d9a50ad19c2
Logan <logwebman@gmail.com>
2025-02-24 13:52:23 +0000

    Update TempCheck.py

--- BackEnd/TempCheck.py
+++ BackEnd/TempCheck.py
@@ -9,14 +9,14 @@ while True:
   temp = sense.get_temperature()
   if temp<=EvilTemp:
     for x in range (10):  #10 iterations of 30 seconds = 5 min
-      time.sleep(30)    #wait 30sec between each check
+      time.sleep(30)      #wait 30sec between each check
       if temp<=EvilTemp:
         lowTemp = True
       else:
         lowTemp = False
-  if lowTemp == True:   #if after 5 min last check still reads True trigger emergency protocol
+  if lowTemp == True:     #if after 5 min last check still reads True trigger emergency protocol
     colour = (255, 0, 0)
-    sense.clear(colour) # This turns all LEDs to red (will be swapped for emergency protocol)
+    sense.clear(colour)   #This turns all LEDs to red (will be swapped for emergency protocol)
   else:
     sense.clear()
-  time.sleep(600)       #check every 10 minutes
+  time.sleep(600)         #check every 10 minutes
