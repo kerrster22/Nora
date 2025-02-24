a556690232ae08fec575c13790210911fcb2f131
Logan <logwebman@gmail.com>
2025-02-24 14:28:30 +0000

    made more durable

--- BackEnd/TempCheck.py
+++ BackEnd/TempCheck.py
@@ -3,21 +3,24 @@ import time
 
 EvilTemp = 5
 sense = SenseHat()
-lowTemp = False
 
 while True:
   sense.clear()
   temp = sense.get_temperature()
   if temp<=EvilTemp:
-    for x in range (10):  #10 iterations of 30 seconds = 5 min
-      time.sleep(30)      #wait 30sec between each check
+    lowTempCount = 0
+
+    for x in range (10):    #10 iterations of 30 seconds = 5 min
+      time.sleep(30)        #wait 30sec between each check
+      temp = sense.get_temperature
       if temp<=EvilTemp:
-        lowTemp = True
-      else:
-        lowTemp = False
-  if lowTemp == True:     #if after 5 min last check still reads True trigger emergency protocol
-    colour = (255, 0, 0)
-    sense.clear(colour)   #This turns all LEDs to red (will be swapped for emergency protocol)
-  else:
+        lowTempCount +=1
+    
+    if lowTempCount >= 6:   #if detected under threshhold more than x times run emergency protocol
+        colour = (225, 0, 0)#red
+        sense.clear(colour)
+    else:
+        sense.clear()
+else:
     sense.clear()
-  time.sleep(600)         #check every 10 minutes
+    time.sleep(600)         #check every 10 minutes
